5052d9cdd5faa3d805aa18a59c1a45bd9f6188aa
Tao Lin <lin.tao.5711y@gmail.com>
2015-12-10 12:16:07 -0500

    added Sphere with volume implementation

---
+++ Sphere.java
@@ -0,0 +1,14 @@
+public class Sphere extends Circle implements Volume{
+
+    public Sphere(double radius, String name){
+	super(radius,name);
+    }
+
+    public String toString(){
+	return "Sphere "+getName()+" with radius of "+getRadius();
+    }
+
+    public double getVolume(){
+	return (4/3)*Math.PI*Math.pow(getRadius(), 3);
+    }
+}
